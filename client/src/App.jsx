8363fc262791a9bcbc885b4a752d1328fbef5fa6
🇲‌🇦‌🇭‌🇲‌🇴‌🇴‌🇩‌ <mahmood.mkhan03@gmail.com>
2026-04-15 17:58:38 +0500

    Add client routing and update assets
    Add react-router-dom dependency and wire up basic client-side routes (Login, Feed, Messages) in App.jsx. Switch favicon to new SVG, add favIcon01.svg and logo01.svg assets, and add a new Layout page. Minor update to main.jsx and package-lock to reflect the dependency change.

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,18 @@
 import React from 'react'
+import { Routes } from 'react-router-dom'
+import Login from './pages/Login'
+import Feed from './pages/Feed'
+import Messages from './pages/Messages'
 
 const App = () => {
   return (
     <>
-    <div>
-    </div>
+    /** The App component serves as the main entry point for the application. */
+      <Routes>
+        <Route path="/" element={<Login />} />
+          <Route index element={<Feed />} />
+          <Route path='messages' element={<Messages />} />  
+      </Routes>
     </>
   )
 }
